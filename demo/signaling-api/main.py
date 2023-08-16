f90754a44d70658bb7fe6051a249534c99b329fd
Denis Loginov <git@dinvlad.dev>
2023-08-16 11:32:21 -0400

    [demo-signaling-api] Use asyncio Barrier and fetch client_id from Google token in demo mode

--- demo/signaling-api/main.py
+++ demo/signaling-api/main.py
@@ -1,24 +1,54 @@
 import asyncio
 import os
-import socket
-from typing import Dict
+from dataclasses import asdict, dataclass
+from enum import Enum
+from typing import Dict, Set
 
-from quart import Quart, Websocket, request, websocket
+import httpx
+from quart import Quart, Websocket, copy_current_websocket_context, websocket
 
 app = Quart(__name__)
 
 # using in-memory store for the demo
 # TODO replace with database lookup in real implementation
-studies = {
-    "study_1": {"client_1", "client_2"},
-    "study_2": {"client_2", "client_3"},
-}
+studies: Dict[str, Set[str]] = {}
 
 # may want to keep in-memory
-connected_clients: Dict[str, Dict[str, Websocket]] = {}
+study_barriers: Dict[str, asyncio.Barrier] = {}
+study_clients: Dict[str, Dict[str, Websocket]] = {}
 
 PORT = os.getenv("PORT", "8000")
 ORIGIN = os.getenv("ORIGIN", f"ws://host.docker.internal:{PORT}")
+
+DEMO_CLIENT_IDS = os.getenv("DEMO_CLIENT_IDS", "").split(",")
+DEMO = len(DEMO_CLIENT_IDS) > 0
+if DEMO:
+    studies = {
+        "1": set(DEMO_CLIENT_IDS[:2]),
+        "2": set(DEMO_CLIENT_IDS[1:]),
+    }
+
+
+class MessageType(Enum):
+    CONNECTED = "connected"
+    ERROR = "error"
+
+
+@dataclass
+class Message:
+    type: MessageType
+    data: str = ""
+    studyId: str = ""
+    clientId: str = ""
+
+    async def send(self, ws=websocket):
+        msg = asdict(self)
+        for key, value in msg.items():
+            if isinstance(value, Enum):
+                msg[key] = value.value
+
+        await ws.send_json(msg)
+        print(f"Sent", msg)
 
 
 @app.websocket("/api/ice")
@@ -27,7 +57,13 @@ async def handler():
     if websocket.headers.get("Origin") != ORIGIN:
         return "Unauthorized", 401
 
-    client_id = websocket.headers.get("oidc_claim_user_id")
+    if DEMO:
+        # when testing, get token subject from Google
+        client_id = await get_subject_id()
+    else:
+        # when running as a Terra service behind Apache proxy,
+        # the proxy will take care of extracting the claim automatically
+        client_id = websocket.headers.get("oidc_claim_user_id")
     if not client_id:
         return "Unauthorized", 401
 
@@ -35,43 +71,79 @@ async def handler():
     msg = await websocket.receive_json()
     study_id = msg["studyId"]
     if not study_id:
-        await websocket.send_json({"error": "Missing studyId"})
+        await Message(MessageType.ERROR, "Missing study ID").send()
         return "Bad Request", 400
+    print(f"Received study ID {study_id} from client {client_id}")
+
+    # get current study and its existing clients, if any
+    # (TODO replace with a real database lookup)
+    study = studies.setdefault(study_id, set())
+    clients = study_clients.setdefault(study_id, {})
 
     # check study access
-    if client_id not in studies.get(study_id, set()):
-        await websocket.send_json({"error": "Client is not part of the study"})
+    if client_id not in study:
+        await Message(
+            MessageType.ERROR, f"Client {client_id} is not part of study {study_id}"
+        ).send()
         return "Forbidden", 403
 
-    print(f"Client {client_id} connected for study {study_id}")
+    # check for duplicate connection
+    if client_id in clients:
+        await Message(
+            MessageType.ERROR,
+            f"Client {client_id} is already connected to study {study_id}",
+        ).send()
+        return "Conflict", 409
 
-    # register the client as connected for the study
-    connected_clients.setdefault(study_id, set())[client_id] = websocket
+    try:
+        # store the current websocket send method for the client
+        @copy_current_websocket_context
+        async def ws_send(msg: dict):
+            await websocket.send_json(msg)
 
-    # check if all participants in a study are connected
-    # and initiate the ICE protocol for all
-    if studies[study_id] == connected_clients[study_id].keys():
-        await asyncio.gather(
-            [
-                ws.send_json({"type": "init", "clientId": cid})
-                for cid, ws in connected_clients[study_id].items()
-            ]
+        clients[client_id] = ws_send
+        print(f"Registered websocket for client {client_id}")
+
+        # using a study-specific barrier,
+        # wait until all participants in a study are connected,
+        # and then initiate the ICE protocol for it
+        async with study_barriers.setdefault(study_id, asyncio.Barrier(len(study))):
+            await Message(MessageType.CONNECTED, clientId=client_id).send()
+            print("All clients have connected:", clients.keys())
+
+            while True:
+                # read the next message and override its client ID
+                # (could be of type 'candidate' or 'credential')
+                msg = await websocket.receive_json()
+                msg["clientId"] = client_id
+                print(f"Received: {msg}")
+
+                # and broadcast it to all of the other participants
+                await asyncio.gather(
+                    *(send(msg) for cid, send in clients.items() if cid != client_id)
+                )
+                print(f"Broadcast: {msg}")
+    except Exception as e:
+        print(f"Terminal error in client {client_id} connection: {e}")
+    finally:
+        del clients[client_id]
+
+
+async def get_subject_id():
+    async with httpx.AsyncClient() as client:
+        res = await client.get(
+            "https://www.googleapis.com/oauth2/v3/tokeninfo",
+            headers={
+                "Authorization": websocket.headers.get("authorization"),
+            },
         )
-
-    while True:
-        # read the next message
-        # (could be of type 'candidate' or 'credential')
-        msg = await websocket.receive_json()
-        print(f"Received: {msg}")
-
-        # and broadcast it to all of the other participants
-        await asyncio.gather(
-            [
-                ws.send_json(msg)
-                for cid, ws in connected_clients[study_id].items()
-                if cid != client_id
-            ]
-        )
+        if res.is_error:
+            await Message(
+                MessageType.ERROR,
+                f"Unable to fetch subject ID from Google: {res.status_code} {str(res.read())}",
+            ).send()
+        else:
+            return res.json()["sub"]
 
 
 if __name__ == "__main__":
