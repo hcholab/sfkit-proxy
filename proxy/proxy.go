7857d743bbf3b823e5a78975e66ecadf9ae8bb2e
Denis Loginov <git@dinvlad.dev>
2024-02-16 16:16:03 -0500

    Improve connection logging

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -221,7 +221,7 @@ func (s *Service) proxyRemoteClient(ctx context.Context, remoteConn net.Conn, lo
 		// proxy requests: local server <- remote client
 		util.Go(ctx, errs, util.Retry(ctx, func() (err error) {
 			nbytes, err := io.Copy(localConn, remoteConn)
-			slog.Debug("Copied local <- remote:", "b", nbytes, "err", err)
+			slog.Debug("Copied local <- remote:", "b", nbytes, "local", localConn.RemoteAddr(), "remote", remoteConn.RemoteAddr(), "err", err)
 			if err == io.EOF {
 				return localEOF
 			} else if err == nil {
@@ -233,7 +233,7 @@ func (s *Service) proxyRemoteClient(ctx context.Context, remoteConn net.Conn, lo
 		// proxy responses: local server -> remote client
 		util.Go(ctx, errs, util.Retry(ctx, func() (err error) {
 			nbytes, err := io.Copy(remoteConn, localConn)
-			slog.Debug("Copied local -> remote:", "b", nbytes, "err", err)
+			slog.Debug("Copied local -> remote:", "b", nbytes, "local", localConn.RemoteAddr(), "remote", remoteConn.RemoteAddr(), "err", err)
 			if err == nil {
 				// err == nil means local connection was closed,
 				// so we should exit and retry after recreating it
