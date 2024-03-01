dd8ce16baa59c020648df4acb862d04746d78b87
Denis Loginov <git@dinvlad.dev>
2024-03-01 17:16:26 -0500

    Add verbose logging of non-QUIC packet handling

--- quic/conn.go
+++ quic/conn.go
@@ -3,6 +3,7 @@ package quic
 import (
 	"context"
 	"errors"
+	"log/slog"
 	"net"
 	"time"
 
@@ -15,25 +16,28 @@ type contextDeadline struct {
 	t      chan time.Time
 }
 
-func newContextDeadline() contextDeadline {
+func newContextDeadline(id string) contextDeadline {
 	ctx, cancel := context.WithCancel(context.Background())
 	d := contextDeadline{
 		ctx:    ctx,
 		cancel: cancel,
 		t:      make(chan time.Time),
 	}
-	go d.monitor()
+	go d.monitor(id)
 	return d
 }
 
-func (d *contextDeadline) monitor() {
+func (d *contextDeadline) monitor(id string) {
 	for t := range d.t {
 		d.cancel() // Cancel the ongoing read if there's any
 
 		if t.IsZero() {
+
 			d.ctx, d.cancel = context.WithCancel(d.ctx)
+			slog.Warn("Deadline cleared:", "id", id, "ctx", d.ctx)
 		} else {
 			d.ctx, d.cancel = context.WithDeadline(d.ctx, t)
+			slog.Warn("Deadline set:", "id", id, "ctx", d.ctx, "interval", t.Sub(time.Now()))
 		}
 	}
 }
@@ -49,7 +53,10 @@ type RawPacketConn struct {
 }
 
 func newRawPacketConn(ctx context.Context, tr *quic.Transport) *RawPacketConn {
-	return &RawPacketConn{ctx, tr, newContextDeadline(), newContextDeadline()}
+	return &RawPacketConn{ctx, tr,
+		newContextDeadline("read with " + tr.Conn.LocalAddr().String()),
+		newContextDeadline("write with " + tr.Conn.LocalAddr().String()),
+	}
 }
 
 func (c *RawPacketConn) GetTransport() *quic.Transport {
@@ -57,17 +64,24 @@ func (c *RawPacketConn) GetTransport() *quic.Transport {
 }
 
 func (c *RawPacketConn) ReadFrom(p []byte) (int, net.Addr, error) {
-	return c.handleOperation(c.readDeadline.ctx,
-		func(ctx context.Context) (int, net.Addr, error) {
-			return c.tr.ReadNonQUICPacket(ctx, p)
+	ctx := c.readDeadline.ctx
+	slog.Debug("Reading non-QUIC packet", "ctx", ctx)
+	return c.handleOperation(ctx,
+		func(ctx context.Context) (n int, addr net.Addr, err error) {
+			n, addr, err = c.tr.ReadNonQUICPacket(ctx, p)
+			slog.Debug("Read non-QUIC packet:", "ctx", ctx, "n", n, "addr", addr, "err", err)
+			return
 		},
 	)
 }
 
 func (c *RawPacketConn) WriteTo(p []byte, addr net.Addr) (int, error) {
+	ctx := c.writeDeadline.ctx
+	slog.Debug("Writing non-QUIC packet", "ctx", ctx, "addr", addr, "n", len(p))
 	n, _, err := c.handleOperation(c.writeDeadline.ctx,
 		func(_ context.Context) (n int, _ net.Addr, err error) {
 			n, err = c.tr.WriteTo(p, addr)
+			slog.Debug("Wrote non-QUIC packet:", "ctx", ctx, "n", n, "addr", addr, "err", err)
 			return
 		},
 	)
@@ -96,6 +110,7 @@ func (c *RawPacketConn) handleOperation(
 		select {
 		case <-ctx.Done():
 			if ctx.Err() == context.Canceled {
+				slog.Warn("Non-QUIC packet op canceled", "ctx", ctx)
 				continue // deadline was changed, retry
 			}
 			return 0, nil, ctx.Err()
